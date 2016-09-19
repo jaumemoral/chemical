67581c7c7a5633e821479e9e4334202b1ecbac39
Jaume Moral <jaume.moral@gmail.com>
2016-09-19 23:11:36 +0200

    Cleaning of autogenerated useless javadoc

--- src/main/java/chemical/SplurthianChemicalSymbolNamer.java
+++ src/main/java/chemical/SplurthianChemicalSymbolNamer.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 public class SplurthianChemicalSymbolNamer implements ChemicalSymbolNamer {
 
-	/* (non-Javadoc)
-	 * @see chemical.ChemicalSymbolName#findValidSymbols(java.lang.String)
-	 */
 	public List<String> findValidSymbols(String name) {
 		List<String> symbols=new ArrayList<String>();
 		char letters[]=name.toCharArray();
@@ -21,23 +18,14 @@ public class SplurthianChemicalSymbolNamer implements ChemicalSymbolNamer {
 		return symbols;
 	}
 
-	/* (non-Javadoc)
-	 * @see chemical.ChemicalSymbolName#checkSymbol(java.lang.String, java.lang.String)
-	 */
 	public boolean checkSymbol(String name, String symbol) {
 		return findValidSymbols(name).contains(symbol);
 	}
 
-	/* (non-Javadoc)
-	 * @see chemical.ChemicalSymbolName#findFirstValidSymbol(java.lang.String)
-	 */
 	public String findFirstValidSymbol(String name) {
 		return Collections.min(findValidSymbols(name));
 	}
 
-	/* (non-Javadoc)
-	 * @see chemical.ChemicalSymbolName#findNumberOfValidSymbols(java.lang.String)
-	 */
 	public int findNumberOfValidSymbols(String name) {
 		return findValidSymbols(name).size();
 	}
